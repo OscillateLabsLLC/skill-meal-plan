dd3039f7fa89e79560958a86a20aae3d6173b76a
Mike Gray <mike@graywind.org>
2022-06-02 21:00:13 -0500

    Bugfix, debugging

--- __init__.py
+++ __init__.py
@@ -26,6 +26,7 @@ class MealPlan(MycroftSkill):
         try:
             self.meals.append(new_meal)  # TODO: Better error handling - what failed?
             with open("meals.json", "w") as f:
+                self.log.debug({"meals": self.meals})
                 f.write(dump({"meals": self.meals}, f))
             self.speak(f"Okay, I've added {new_meal} to your list of meals. Yum!")
         except Exception as e:
