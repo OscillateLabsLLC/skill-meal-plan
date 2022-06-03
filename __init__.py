c4e31c40eaba540a5743d3c5b46069c792cb3b4c
Mike Gray <mike@graywind.org>
2022-06-02 23:41:52 -0500

    Don't overwrite changes

--- __init__.py
+++ __init__.py
@@ -13,8 +13,9 @@ class MealPlan(MycroftSkill):
         MycroftSkill.__init__(self)
         self.meals_location = Path(Path.home()) / ".config/mycroft/skills/meal-plan-skill/meals.json"
         self.meals_location.touch(exist_ok=True)
-        with open(self.meals_location, "w") as f:
-            dump(INITIAL_MEALS, f)
+        with open(self.meals_location, "a") as f:
+            if not f.read():
+                dump(INITIAL_MEALS, f)
 
     def initialize(self):
         self.meals_location = Path(Path.home()) / ".config/mycroft/skills/meal-plan-skill/meals.json"
