35523f72e7eeed8cc62180e08b6b2ef05dd6010f
Mike Gray <mike@graywind.org>
2022-06-02 23:44:39 -0500

    Improvements

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ class MealPlan(MycroftSkill):
         MycroftSkill.__init__(self)
         self.meals_location = Path(Path.home()) / ".config/mycroft/skills/meal-plan-skill/meals.json"
         self.meals_location.touch(exist_ok=True)
-        with open(self.meals_location, "a") as f:
+        with open(self.meals_location, "w+") as f:
             if not f.read():
                 dump(INITIAL_MEALS, f)
 
