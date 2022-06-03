d318aa76e59302033f5639d659eb5acc258ec8e1
Mike Gray <mike@graywind.org>
2022-06-02 23:20:35 -0500

    Move init actions to appropriate locations

--- __init__.py
+++ __init__.py
@@ -1,36 +1,27 @@
 from json import loads, dump
-from os.path import isfile
 from pathlib import Path
 from random import choice
 from typing import Dict, List, Union
 
 from mycroft import MycroftSkill, intent_file_handler
 
+INITIAL_MEALS = {"Spaghetti and meatballs", "Toasted sandwiches and tomato soap", "Chicken noodle soup"}
+
 
 class MealPlan(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.meal_location = Path(Path.home()) / ".config/mycroft/skills/meal-plan-skill/meals.json"
+        self.meal_location.touch(exist_ok=True)
 
     def initialize(self):
         self.meal_location = Path(Path.home()) / ".config/mycroft/skills/meal-plan-skill/meals.json"
-        self.first_run = self._first_run()
         self.meals: Union[List[str], None] = self._get_meals().get("meals")
         self._save_meals()
 
-    def _first_run(self) -> bool:
-        return not isfile(self.meal_location)
-
     def _get_meals(self) -> Dict[str, List[str]]:
-        if self.first_run is True:
-            with open(Path("/opt/mycroft/skills/meal-plan-skill/meals.json"), "r") as f:
-                self.meal_location.touch(exist_ok=True)
-                with open(self.meal_location, "w") as file:
-                    file.write(f.read())
-                    meals = loads(f.read())
-            self.first_run = False
-        else:
-            with open(self.meal_location, "w") as file:
-                meals = loads(file.read())
+        with open(self.meal_location, "w") as file:
+            meals = loads(file.read())
         return meals
 
     def _save_meals(self) -> None:
@@ -44,12 +35,12 @@ class MealPlan(MycroftSkill):
         self.speak(choice(self.meals))
 
     @intent_file_handler("add.meal.intent")
-    def add_meal(self):
+    def handle_add_meal(self):
         # Wait for a response and add it to meals.json
         new_meal = self.get_response("add.meal")
         try:
             self.log.info(f"Adding a new meal: {new_meal}")
-            self.meals.append(new_meal)  # TODO: Better error handling - what failed?
+            self.meals.append(new_meal)
             self._save_meals()
             self.speak(f"Okay, I've added {new_meal} to your list of meals. Yum!")
         except Exception as e:
@@ -57,11 +48,11 @@ class MealPlan(MycroftSkill):
             self.speak("I wasn't able to add that meal. I'm sorry.")
 
     @intent_file_handler("remove.meal.intent")
-    def remove_meal(self):
+    def handle_remove_meal(self):
         self.speak("I'm sorry, I can't remove meals yet, but I'm learning all the time.")  # TODO:
 
     @intent_file_handler("list.meal.intent")
-    def list_meals(self):
+    def handle_list_meals(self):
         if len(self.meals) > 10:
             confirm = self.get_response("Are you sure? You have more than 10 meals listed. This may take some time.")
             if confirm.lower() not in (
