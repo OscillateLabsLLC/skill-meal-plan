386b8537faf85df73a3f9846b30c7e84c4675029
Mike Gray <mike@graywind.org>
2024-02-06 23:09:08 -0600

    test fixes

--- test/test_main.py
+++ test/test_main.py
@@ -4,7 +4,8 @@ from ovos_plugin_manager.skills import find_skill_plugins
 
 
 def test_skill_is_a_valid_plugin():
-    assert "skill-meal-plan.mikejgray" in find_skill_plugins().keys()
+    skills = ",".join(find_skill_plugins().keys())
+    assert "skill-meal-plan.mikejgray" in skills
 
 
 if __name__ == "__main__":
